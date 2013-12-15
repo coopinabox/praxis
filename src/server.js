7acb87e3e2a4e2c943a90294ba6b5fdf0714d83a
Michael Williams <dinosaur@riseup.net>
2013-12-15 00:52:03 -0800

    add .nodemon ignore, edit .gitignore, change cache of ecstatic server during development

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 var feathers = require('feathers');
 var ecstatic = require('ecstatic');
 
+var isProd = (process.env.NODE_ENV === 'production')
+
 var app = feathers()
   .configure(feathers.socketio())
   .use('/tasks', require('./tasks/service'))
-  .use(ecstatic(__dirname + '/../static'))
+  .use(ecstatic({
+    root: __dirname + '/../static',
+    cache: (isProd ? 3600 : 0),
+  }))
   .listen(5000);
 
 module.exports = app;
