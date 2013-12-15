a4bcbc1cfb63f308467f06bbe5c6833f07eddd6e
Michael Williams <dinosaur@riseup.net>
2013-12-14 16:25:38 -0800

    add index.header.html to header, close #5

--- src/client.js
+++ src/client.js
@@ -2,6 +2,8 @@
 require('backbone').$ = $;
 
 var fs = require('fs');
+var headerHtml = fs.readFileSync(__dirname + '/index.header.html');
+$('body > header').html(headerHtml);
 
 var Tasks = require('./tasks/collection.client');
 var tasks = new Tasks();
