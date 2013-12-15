4ae9065bf5ae4d9fb12e3e740ebdab30ad1c47fb
Michael Williams <dinosaur@riseup.net>
2013-12-14 20:46:51 -0800

    improved task view styling, errors no longer much up forms

--- src/client.js
+++ src/client.js
@@ -10,4 +10,4 @@ $('body > header').html(headerHtml);
 var Tasks = require('./tasks/collection.client');
 var tasks = new Tasks();
 var TasksView = require('./tasks/view');
-var tasksView = TasksView(tasks, '#tasks');
+var tasksView = TasksView(tasks, '.tasks');
--- src/tasks/model.server.js
+++ src/tasks/model.server.js
@@ -3,6 +3,10 @@ var db = require('../db');
 module.exports = db.Model.extend({
   tableName: 'tasks',
   validation: require('./validation'),
+  defaults: {
+    name: "",
+    description: "",
+  },
   initialize: function () {
     var self = this;
     this.on('saving', function () {
