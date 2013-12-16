8d3137b16abf7a36c548e2979b4cd62b2018f6c9
James Myers <jfmyers9@gmail.com>
2013-12-15 17:53:29 -0800

    Newly created tasks are not saved to the server until they pass client side validations, server-side tasks cannot be created with invalid attributes

--- test/server/tasks/TaskList.js
+++ test/server/tasks/TaskList.js
@@ -11,23 +11,28 @@ describe("TaskList", function () {
     beforeEach(function () {
       taskList.reset();
     });
-    it("should have correct defaults", function (done) {
-      taskList.create({}).then(function (task) {
+
+    it("should create valid tasks", function (done) {
+      taskList.create({
+        'name': 'valid_name',
+        'description': 'valid_description',
+      }).then(function (task) {
         task.id
           .should.be.type('number');
         task.get('name')
           .should.be.type('string')
-          .and.equal('');
+          .and.equal('valid_name');
         task.get('description')
           .should.be.type('string')
-          .and.equal('');
+          .and.equal('valid_description');
         done();
       });
     });
+
     it("should not allow invalid data", function (done) {
       taskList.create({
         "name": "areallylongnamethatissolong",
-        "description": "",
+        "description": "valid_description",
       }).then(function (data) {
         false.should.be.ok;
       }).catch(function (error) {
