4d55c0520f0f61c69bd6a348c4f24b6c77439303
Michael Williams <dinosaur@riseup.net>
2013-12-15 20:55:12 -0800

    move page elements from index.html to client.js

--- src/client.js
+++ src/client.js
@@ -1,19 +1,36 @@
 "use strict";
 
-var version = require('../package.json').version;
-
-var $ = jQuery = require('jquery/dist/jquery')(window);
+var $ = require('jquery/dist/jquery')(window);
 require('bootstrap/dist/js/bootstrap');
 // attach jquery to backbone
 require('backbone').$ = $;
 
 var fs = require('fs');
-var headerHtml = fs.readFileSync(__dirname + '/index.header.html');
-$('head > title').text(version);
-$('body > header').html(headerHtml);
-$('.brand').text(version);
 
-var TaskList = require('tasks/TaskList.client');
-var taskList = new TaskList();
-var TaskListView = require('tasks/TaskListView');
-var taskListView = TaskListView(taskList, '.tasks');
+$(function() {
+
+  // setup header
+  $('body').append(
+      '<header>' + 
+        fs.readFileSync(__dirname + '/index.header.html') +
+      '</header>'
+      );
+
+  // setup main with tasks element
+  $('body').append(
+      '<main>' +
+        '<div class="tasks"></div>' +
+      '</main>'
+      );
+
+  // show version in title and brand
+  var version = require('../package.json').version;
+  $('head > title').text(version);
+  $('.brand').text(version);
+
+  // setup tasks view
+  var TaskList = require('tasks/TaskList.client');
+  var taskList = new TaskList();
+  var TaskListView = require('tasks/TaskListView');
+  var taskListView = TaskListView(taskList, '.tasks');
+});
